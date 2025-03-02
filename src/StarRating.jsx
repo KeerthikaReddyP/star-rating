ad09bd0ba834af7c1a41e077cab6ebbbb7dd19ab
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-03-02 22:32:02 +0530

    SOmething (too lazy to give a name)

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,8 +1,13 @@
-const StarRating = ({starCount=5}) => {
+/* eslint-disable react/prop-types */
+const StarRating = ({ starCount = 5 }) => {
   return (
     <div className="flex flex-col justify-center items-center h-screen relative">
       <h1 className="text-2xl font-bold absolute top-6">Star rating</h1>
-      <div className="flex justify-center items-center text-3xl">&#9733;</div>
+      <div className="flex justify-center items-center text-3xl">
+        {new Array(starCount).fill(0).map((a,ind)=>{
+            return <span key={ind}>&#9733;</span>
+        })}
+      </div>
     </div>
   );
 };
