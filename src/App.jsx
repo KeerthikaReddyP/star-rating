4fb9b9eafc2659e748730f3442d24b00562b42c8
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-03-02 22:14:18 +0530

    StartRating component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
+import StarRating from "./starRating"
 
 function App() {
 
-  return (
-    <div className="flex flex-col justify-center items-center h-screen relative">
-      <h1 className="text-2xl font-bold absolute top-6">Star rating</h1>
-      <div className="flex justify-center items-center text-3xl">&#9734;</div>
-    </div>
-  )
+  return <StarRating />
 }
 
 export default App
---
+++ src/StarRating.jsx
@@ -0,0 +1,5 @@
+const StarRating=()=>{
+    return <div>Im a star</div>
+}
+
+export default StarRating;
