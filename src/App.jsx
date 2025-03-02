4098c08c26b458f87c985ecd2d5e2ee4fe94b0ae
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-03-02 16:37:56 +0530

    Increased star size

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen relative">
       <h1 className="text-2xl font-bold absolute top-6">Star rating</h1>
-      <div className="flex justify-center items-center">&#9734;</div>
+      <div className="flex justify-center items-center text-3xl">&#9734;</div>
     </div>
   )
 }
