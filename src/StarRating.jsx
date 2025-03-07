74ff608462d088046cd3e666d5ba88706b47e900
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-03-07 21:27:50 +0530

    Formatted

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 
 /* eslint-disable react/prop-types */
 const StarRating = ({ starCount = 10 }) => {
-
-  const [starValue,setStarValue]=useState();
-  console.log(starValue);
+  const [starValue, setStarValue] = useState();
 
   return (
     <div className="flex flex-col justify-center items-center h-screen relative">
@@ -12,7 +10,11 @@ const StarRating = ({ starCount = 10 }) => {
       <div className="flex justify-center items-center text-3xl">
         {new Array(starCount).fill(0).map((a, ind) => {
           return (
-            <span className={ind<starValue?"text-yellow-400":""} key={ind} onClick={()=>setStarValue(ind+1)}>
+            <span
+              className={ind < starValue ? "text-yellow-400" : ""}
+              key={ind}
+              onClick={() => setStarValue(ind + 1)}
+            >
               &#9733;
             </span>
           );
