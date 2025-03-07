bcfaf3378f485480d6f76135d8de29f22eae47fd
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-03-07 22:29:17 +0530

    One more thing I wasn't aware of  :/

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 /* eslint-disable react/prop-types */
 const StarRating = ({ starCount = 10 }) => {
   const [starValue, setStarValue] = useState();
-  const [hoverValue, setHovervalue]=useState();
+  const [hoverValue, setHovervalue]=useState(0);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen relative">
@@ -12,7 +12,7 @@ const StarRating = ({ starCount = 10 }) => {
         {new Array(starCount).fill(0).map((a, ind) => {
           return (
             <span
-              className={ind < starValue || ind<hoverValue ? "text-yellow-400" : ""}
+              className={hoverValue==0 && ind < starValue || ind<hoverValue ? "text-yellow-400" : ""}
               key={ind}
               onClick={() => setStarValue(ind + 1)}
               onMouseEnter={()=>setHovervalue(ind+1)}
